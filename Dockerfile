401d6222c3a37e9d1d1658f136b5d4c503a16254
xenu <xenucode@gmail.com>
2024-08-11 11:43:34 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:8.0 AS base
+USER $APP_UID
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["dyn-dns.csproj", "./"]
+RUN dotnet restore "dyn-dns.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "dyn-dns.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "dyn-dns.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "dyn-dns.dll"]
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using Amazon;
+using Amazon.Route53;
+using Amazon.Route53.Model;
+using Amazon.Runtime;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace aws_dyndns;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LogFiles", "Log.txt"),
+                rollingInterval: RollingInterval.Month,
+                outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level}] {Message}{NewLine}{Exception}")
+            .CreateLogger();
+        
+        // Replace with your AWS credentials
+        TextReader textReader = new StreamReader("config.json");
+        Config? config =  JsonConvert.DeserializeObject<Config>(textReader.ReadToEnd());
+        AWSCredentials credentials = new BasicAWSCredentials(config.AccessKey,config.SecretKey);
+        AmazonRoute53Client route53Client = new AmazonRoute53Client(credentials, RegionEndpoint.EUCentral1);
+        ChangeBatch changeBatch = new ChangeBatch();
+        var we = new HttpClient().GetAsync("https://api.ipify.org").Result.Content.ReadAsStream();
+        TextReader te = new StreamReader(we);
+        var res = new ResourceRecordSet
+        {
+            Name = config.Domain, // Your domain name
+            Type = RRType.A,
+            TTL = 60, // Time to live in seconds
+            ResourceRecords = 
+            {
+                new ResourceRecord
+                {
+                    Value =te.ReadToEnd()
+                }
+                
+            }
+        };
+        changeBatch.Changes.Add(new Change(ChangeAction.UPSERT,res));
+        ChangeResourceRecordSetsRequest changeResourceRecordSetsRequest = new ChangeResourceRecordSetsRequest( config.HostedZoneId , changeBatch);
+        var r= route53Client.ChangeResourceRecordSetsAsync(changeResourceRecordSetsRequest);
+        Console.WriteLine(r.Result.HttpStatusCode);
+        Log.Information(r.Result.HttpStatusCode.ToString());
+    }
+}
+
+class Config
+{
+    public Config()
+    {
+    }
+
+    public string AccessKey { get; set; }
+    public string SecretKey { get; set; }
+    public string Domain { get; set; }
+    public string HostedZoneId { get; set; }
+}
